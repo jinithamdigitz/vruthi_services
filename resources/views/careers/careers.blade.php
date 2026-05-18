0ca112379716295e3ea8b4b417e53ad91ed39f83
Iam shafi <iamshafimc@gmail.com>
2026-05-19 03:52:27 +0530

    working on careers page

--- resources/views/careers/careers.blade.php
+++ resources/views/careers/careers.blade.php
@@ -802,21 +802,21 @@
             <i class="bi bi-person-workspace"></i>
           </div>
           <div class="job-card__body">
-            <h3 class="job-card__title">Senior Architect ggg</h3>
-            <p class="job-card__desc">Lead architectural projects from concept to completion and mentor junior team members.</p>
+            <h3 class="job-card__title"> {{ $careerJob->title }}</h3>
+            <p class="job-card__desc">{{ $careerJob->short_description }}</p>
             <div class="job-tags">
-              <span class="job-tag">Architecture</span>
-              <span class="job-tag">5+ Years Experience</span>
+              <span class="job-tag">{{ $careerJob->department }}</span>
+              <span class="job-tag">{{ $careerJob->experience }}  Years Experience</span>
             </div>
           </div>
           <div class="job-card__meta">
             <div class="job-meta-item">
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"/><circle cx="12" cy="10" r="3"/></svg>
-              Dubai, UAE
+             {{ $careerJob->location }}
             </div>
             <div class="job-meta-item">
               <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><rect x="2" y="7" width="20" height="14" rx="2"/><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"/></svg>
-              Full-time
+              {{ ucfirst($careerJob->employment_type) }}
             </div>
             <a href="#careers-contact-strip" class="btn-apply">Apply Now &nbsp;<i class="bi bi-arrow-right"></i></a>
           </div>
